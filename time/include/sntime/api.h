035a60e3f577615391baac141200f5ec99fd2eaf
kshku <1211shree@gmail.com>
2026-06-10 12:09:42 +0530

    refactor: rename SN_API → SN_TIME_API

--- time/include/sntime/api.h
+++ time/include/sntime/api.h
@@ -1,9 +1,11 @@
+#pragma once
+
 #include <sncore/api_common.h>
 
 #if defined(SN_TIME_STATIC)
-    #define SN_API
+    #define SN_TIME_API
 #elif defined(SN_EXPORT)
-    #define SN_API SN_API_HELPER_EXPORT
+    #define SN_TIME_API SN_API_HELPER_EXPORT
 #else
-    #define SN_API SN_API_HELPER_IMPORT
+    #define SN_TIME_API SN_API_HELPER_IMPORT
 #endif
